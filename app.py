c725fd12579c6abc6d545a6ab0edc3126ab44329
Matthew Saad <msaad02@outlook.com>
2025-03-24 19:54:06 -0400

    test 4o mini deployment

--- app.py
+++ app.py
@@ -1,17 +1,55 @@
 import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
+import requests
+import os
 
-st.title("Hello Streamlit-er 👋")
-st.markdown(
-    """ 
-    This is a playground for you to try Streamlit and have fun. 
+load_dotenv()
 
-    **There's :rainbow[so much] you can build!**
-    
-    We prepared a few examples for you to get started. Just 
-    click on the buttons above and discover what you can do 
-    with Streamlit. 
-    """
-)
+client = OpenAI()
 
-if st.button("Send balloons!"):
-    st.balloons()
+
+st.write("The red flag jobs app")
+
+st.caption("Note that this demo is connected to GPT-4o-mini")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "assistant", "content": "Let's start chatting! 👇"}]
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("What is up?"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        assistant_response = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=[
+                {"role": "developer", "content": "You are a helpful assistant."},
+                {"role": "user", "content": prompt}
+            ],
+            stream=True
+        )
+
+        for chunk in assistant_response:
+            res = chunk.choices[0].delta.content
+            if res:
+                full_response += res
+                # Add a blinking cursor to simulate typing
+                message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
